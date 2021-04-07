4bcb37531edd0cb7df2811fb095238fc3119add2
Pierre Kisters <pierrekisters@gmail.com>
2021-04-07 15:24:59 +0200

    functional alternative for beforeAll and afterAll

--- build.sbt
+++ build.sbt
@@ -60,7 +60,7 @@ lazy val core = projectMatrix.in(file("core"))
   .settings(
     libraryDependencies ++= Seq(
       "org.scalameta" %%% "munit" % "0.7.23",
-      "org.typelevel" %%% "cats-effect-kernel" % "3.0.1",
+      "org.typelevel" %%% "cats-effect-std" % "3.0.1",
       "org.typelevel" %%% "cats-effect" % "3.0.1" % Test,
     ),
 
--- core/src/main/scala/munit/TaglessFinalSuite.scala
+++ core/src/main/scala/munit/TaglessFinalSuite.scala
@@ -1,9 +1,14 @@
 package munit
 
+import cats.effect.kernel.{Async, Deferred, Resource}
+import cats.syntax.flatMap._
+
 import scala.concurrent.Future
 import scala.reflect.ClassTag
 
-abstract class TaglessFinalSuite[F[_]](implicit private val tag: ClassTag[F[_]])
+abstract class TaglessFinalSuite[F[_]](implicit
+                                       private val AsyncF: Async[F],
+                                       private val tag: ClassTag[F[_]])
   extends FunSuite
     with TaglessFinalAssertions[F]
     with TaglessFinalFunFixtures[F] {
@@ -18,4 +23,18 @@ abstract class TaglessFinalSuite[F[_]](implicit private val tag: ClassTag[F[_]])
 
   override def munitValueTransforms: List[ValueTransform] =
     super.munitValueTransforms :+ fTransform
+
+  private val afterAllF: Deferred[F, F[Unit]] = Deferred.unsafe
+  private val beforeAllTest: Test = new Test("before all", () =>
+    toFuture(aroundAll().allocated.flatMap(e => afterAllF.complete(e._2))))
+  private val afterAllTest: Test = new Test("after all", () => toFuture(afterAllF.get.flatten))
+
+  override def munitTests(): Seq[Test] = beforeAllTest +: super.munitTests() :+ afterAllTest
+
+  /**
+    * Resource is allocated once before all test cases and before all suite-local fixtures are setup.
+    * An error in the allocation method aborts the test suite.
+    * The resource is closed after all test cases and after all suite-local fixtures have been tear down.
+    */
+  def aroundAll(): Resource[F, Unit] = Resource.unit
 }
